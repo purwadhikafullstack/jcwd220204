3324047b46b46b2b711ab7b3191c71259928e2e3
Peppi <111718022+DonnuTs69@users.noreply.github.com>
2023-01-10 16:53:08 +0700

    Debugging 1 (#31)
    * button checkout
    
    * fix navigate
    
    * debugging 1
    
    * debugging
    
    * debugging UI
    
    * UI BUG
    
    * register
    
    * env
    
    * UI BUG
    
    * delete trash
    
    * ENV

--- projects/server/api/index.js
+++ projects/server/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://nginep-f1ba2-default-rtdb.firebaseio.com",
+  baseURL: process.env.BASE_URL,
 })
 export { axiosInstance }
